e0ce16d781d7dc422a0478983b1f2481867f12f9
majd eddine ben tahar <majdeddinebentahar@gmail.com>
2020-08-11 09:06:51 +0100

    Updates

---
+++ precache-manifest.1e5e088565546f41fb7a12a6db801e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278e54173a4454dd5e144d475c8d76c8",
+    "url": "/react-with-chartjs/index.html"
+  },
+  {
+    "revision": "427819fbec93e4c44e6f",
+    "url": "/react-with-chartjs/static/css/main.538dab10.chunk.css"
+  },
+  {
+    "revision": "8fbc371274ce9e93e086",
+    "url": "/react-with-chartjs/static/js/2.0c0d1f56.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/react-with-chartjs/static/js/2.0c0d1f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427819fbec93e4c44e6f",
+    "url": "/react-with-chartjs/static/js/main.56f23e0f.chunk.js"
+  },
+  {
+    "revision": "f0883c560d711dcea941",
+    "url": "/react-with-chartjs/static/js/runtime-main.c2872e77.js"
+  }
+]);
